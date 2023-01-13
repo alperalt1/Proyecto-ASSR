db4377b1aca747a09721e8421a3def43a1cc3059
alperalt1 <alperalt@espol.edu.ec>
2023-01-13 01:10:52 -0500

    update new

--- src/app.js
+++ src/app.js
@@ -1,47 +1,44 @@
 const express = require('express');
-const { engine } = require('express-handlebars');
-//const myconnection = require('express-myconnection');
-const mysql = require('mysql');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-
-const connection = require('../src/config/databaseCon');
-
-
-
+const { auth, requiresAuth } = require('express-openid-connect');
 const app = express();
-app.set('port',4000);
+require('dotenv').config()
 
-app.set('views', __dirname + '/views');
-app.engine('.hbs', engine({
-    extname: '.hbs',
-}));
-app.set('view engine','hbs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
-
-app.listen(app.get('port'), ()=> {
-    connection.connect(function(err){
-        if (err) throw err;
-        console.log('Database connected');
-    })
-});
-
-console.log('listening on port',app.get('port'));
-
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    secret: process.env.SECRET,
+  };
   
+app.use(auth(config));
 
-app.get('/', (req,res)=>{
-    res.render('home');
+// req.oidc.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(
+    req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out'
+  )
 });
-//clave db
-//xRMnVLP690UfMGPA
-//link db
-//mongodb+srv://alperalt:<password>@cluster0.vyaymhz.mongodb.net/?retryWrites=true&w=majority
+
+// The /profile route will show the user profile as JSON
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+});
+
+app.listen(3000, function() {
+  console.log('Listening on http://localhost:3000');
+});
+
+
+
+// app.listen(app.get('port'), ()=> {
+//     connection.connect(function(err){
+//         if (err) throw err;
+//         console.log('Database connected');
+//     })
+// });
+
+
+
+
